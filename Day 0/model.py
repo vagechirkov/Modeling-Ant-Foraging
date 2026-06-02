d8e8e25a976d3124f5285a47a6927b8aa320aab4
valery <vagechirkov@gmail.com>
2026-06-02 17:40:48 +0200

    solutions to the exercises in class

--- Day 0/model.py
+++ Day 0/model.py
@@ -10,11 +10,12 @@ class TreeCell(CellAgent):
 
     def step(self):
         if self.condition == "On Fire":
-            for agent in self.cell.get_neighborhood(radius=1).agents:
+            for agent in self.cell.get_neighborhood(radius=2).agents:
                 if isinstance(agent, TreeCell) and agent.condition == "Fine":
                     agent.condition = "On Fire"
             
             self.condition = "Burned Out"
+
 
 class ForestFire(Model):
     def __init__(self, width=50, height=50, density=0.6):
